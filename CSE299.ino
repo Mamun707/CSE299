febf8fae591578f497784a89d9f5e9a09e878495
MD MAMUN HOWLADER <43126279+Mamun707@users.noreply.github.com>
2019-07-08 14:16:49 +0600

    Update CSE299.ino

--- CSE299.ino
+++ CSE299.ino
@@ -1,83 +1,44 @@
-
-const int trigPin = 7;
-const int echoPin = 4;
-int buzz = 10;
-long duration;
-int distance;
-
-// the setup function runs once when you press reset or power the board
+#include <NewPing.h>            // ultrasound sensor library
+#include <Adafruit_GFX.h>       // for writing text/graphics to screen
+#include <Adafruit_SSD1306.h>   // driver library for oleds with 1306 driver chip
+ 
+#define OLED_Address 0x3C       // I2C address of oled, if this doesn't work try 0x3D
+Adafruit_SSD1306 oled(128,64);  // create our screen object setting resolution to 128x64
+ 
+#define NUM_OF_READINGS 10      // number of readings to take before using highest for display
+                                // This gives a less jittery stable value
+                                // But at the expense of taking longer to update display
+                                // A value of 10 will be 0.5s per update to display
+ 
+#define TRIGGER_PIN  12         // Arduino pin tied to trigger pin on the ultrasonic sensor.
+#define ECHO_PIN     11         // Arduino pin tied to echo pin on the ultrasonic sensor.
+ 
+NewPing sonar(TRIGGER_PIN, ECHO_PIN); // NewPing setup of pins
 void setup() {
-  // initialize digital pin 13 as an output.
-  pinMode(trigPin, OUTPUT);
-  pinMode(echoPin, INPUT);
-  pinMode(13, OUTPUT);
-  pinMode(2, OUTPUT);
-  Serial.begin(9600);
-}
-
-// the loop function runs over and over again forever
-void loop() 
-{
-// Clears the trigPin
-digitalWrite(trigPin, LOW);
-delayMicroseconds(2);
-// Sets the trigPin on HIGH state for 10 micro seconds
-digitalWrite(trigPin, HIGH);
-delayMicroseconds(10);
-digitalWrite(trigPin, LOW);
-// Reads the echoPin, returns the sound wave travel time in microseconds
-duration = pulseIn(echoPin, HIGH);
-// Calculating the distance
-distance= duration*0.034/2;
-// Prints the distance on the Serial Monitor
-if(distance <= 50 && distance >= 20)
-{
-  digitalWrite(13, HIGH); 
-  
- // digitalWrite(13, LOW);
-  // turn the LED on (HIGH is the voltage level)
-
-}
-
-
-else
-{ 
-  digitalWrite(13, LOW);    // turn the LED off by making the voltage LOW
-             // wait for a second    
-}
-
-
-if(distance <= 20)
-{
-    digitalWrite(2, HIGH);
+  oled.begin(SSD1306_SWITCHCAPVCC, OLED_Address);     // init the display
+} 
     
-    tone(buzz, 2000);
-    delay(100);
-    noTone(buzz);
-    delay(100);
-    
-    tone(buzz, 2000);
-    delay(100);
-    noTone(buzz);
-    delay(100);
-    
-    tone(buzz, 2000);
-    delay(100);
-    noTone(buzz);
-        
-    tone(buzz, 2000);
-    delay(100);
-    noTone(buzz);
-    delay(100);
+long Distance;                  // Highest reading in the last samples to display
+long ThisReading;               // and last red current reading
+ 
+void loop() {
+  // Take readings, go with highest, from experimentation the highest from a range
+  // of samples is usaully the correct one with lower values being spurious
+  Distance=0;
+  for(int i=0;i<NUM_OF_READINGS;i++)
+  {
+    delay(50);                     // Wait 50ms between pings (20 pings/sec). 29ms should be the shortest delay between pings.
+    ThisReading=sonar.ping_cm();
+    if(ThisReading>Distance)
+      Distance=ThisReading;
+  }
+  // Display result to display
+  oled.clearDisplay();
+  oled.setTextColor(WHITE);
+  oled.setTextSize(2);
+  oled.setCursor(0,0);
+  oled.print("Distance :");
+  oled.print(Distance);
+  oled.println("cm");
+  oled.display();
 }
-
-else
-{ 
-  digitalWrite(2, LOW);    // turn the LED off by making the voltage LOW
-             // wait for a second    
-}
-}
-
-
-
-
